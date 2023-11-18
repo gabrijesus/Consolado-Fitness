14fe2ce6f8aa3e4edb0d62f274fa606f14e177e0
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-11-18 20:12:13 -0300

    Ajustes de conexão para localhost

--- php/conexao.php
+++ php/conexao.php
@@ -1,9 +1,9 @@
 
 <?php
 // Dados do banco de dados e do servidor
-$host = "fdb1032.awardspace.net";
-$username = "4401063_consuladofitness";
-$password = "Consul@do15";
+$host = "localhost";
+$username = "root";
+$password = "";
 $database = "4401063_consuladofitness";
 
 $conn = mysqli_connect($host, $username, $password, $database);

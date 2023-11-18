e17f498eb50f484f7018f9493753464bd0822850
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-11-18 20:12:56 -0300

    Atualização das consultas SQL

--- php/queriesSql.php
+++ php/queriesSql.php
@@ -1,5 +1,5 @@
 <?php 
-    define("QUERY_SELECT_EXERCICIOS", "SELECT * FROM exercicios WHERE modalidade = ?");
+    define("QUERY_SELECT_EXERCICIOS", "SELECT nome_exercicio FROM exercicio WHERE (modalidade = ? OR grupo_muscular = 'Abdomen') AND grupo_muscular = ? ORDER BY RAND() LIMIT ?");
     define("QUERY_INSERT_USER", "INSERT INTO usuarios (nome_usuario, email, senha, nivel_treino, preferencia_treino, data_nascimento, sexo) VALUES (?,?,?,?,?,?,?)");
     define("QUERY_INSERT_FICHA_TREINO", "INSERT INTO ficha_de_treino (nome_ficha_treino, exercicios) VALUES (?,?)");
     define("QUERY_SELECT_USER", "SELECT * FROM usuarios WHERE id_usuario = ?");

1b9b211fa814d47a80473fc4a44868af95e3ff4f
gabrijesus <111756782+gabrijesus@users.noreply.github.com>
2023-11-20 23:48:15 -0300

    .

--- js/global.js
+++ js/global.js
@@ -2,9 +2,9 @@ function menuResponsivo() {
     var mobile = document.querySelector('.mobile');
         if (mobile.classList.contains('open')) {
             mobile.classList.remove('open');
-            document.querySelector('.icon_botao').src = "../imagens/menu02.png"
+            document.querySelector('.icon_botao').src = "imagens/menu02.png"
         } else {
             mobile.classList.add('open');
-            document.querySelector('.icon_botao').src = "../imagens/fechar.png"
+            document.querySelector('.icon_botao').src = "imagens/fechar.png"
         }
 }

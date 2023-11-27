be86974eddfaf47f93c0003e67f018b0992d1d2d
Deyvid Gondim de Jesus <Deyvidgondim10@gmail.com>
2023-11-27 00:12:02 -0300

    Finalização da página de biblioteca de exercícios

--- php/bibliotecaExercicios.php
+++ php/bibliotecaExercicios.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // if (!isset($_SESSION['user_id'])) {
-    // Redirecionar para a página de login ou fazer qualquer outra ação necessária
+// Redirecionar para a página de login ou fazer qualquer outra ação necessária
 //     header("Location: login.php");
 //     exit();
 // }
@@ -9,7 +9,7 @@ session_start();
 include "conexao.php";
 include "queriesSql.php";
 
-$result = $conn->query(QUERY_SELECT_TODOS_EXERCICIOS);
+$result = $conn->query(QUERY_SELECT_TODOS_EXERCICIOS . " ORDER BY nome_exercicio ASC");
 
 if ($_SERVER["REQUEST_METHOD"] === "GET") {
     if (isset($_GET["pesquisa"]) && $_GET["pesquisa"] !== "") {
@@ -130,7 +130,7 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
                 <label for="filtro">Filtrar por:</label>
                 <select class="select_campo" id="filtro" name="filtro" onchange="this.form.submit()">
                     <optgroup label="Grupos musculares">
-                        <option value=""></option>
+                        <option value="">Todos</option>
                         <option <?php echo (isset($_GET['filtro']) && $_GET['filtro'] == 'abdomen') ? 'selected' : ''; ?> value="abdomen">Abdomen</option>
                         <option <?php echo (isset($_GET['filtro']) && $_GET['filtro'] == 'biceps') ? 'selected' : ''; ?> value="biceps">Biceps</option>
                         <option <?php echo (isset($_GET['filtro']) && $_GET['filtro'] == 'triceps') ? 'selected' : ''; ?> value="triceps">Triceps</option>
@@ -170,9 +170,9 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
                 <?php
                 foreach ($result as $exercicio) {
                     echo '<li>';
-                    echo '    <div class="card" onclick="abrirModal(\'' . $exercicio["nome_exercicio"] . '\', \'' . $exercicio["urlVideo"] . '\')">';
+                    echo '    <div class="card" onclick="abrirModal(\'' . $exercicio["nome_exercicio"] . '\', \'' . $exercicio["grupo_muscular"] . '\', \'' . $exercicio["urlImagem"] . '\', \'' . $exercicio["urlVideo"] . '\')">';
                     echo '        <h2 class="titulo_card">' . $exercicio["nome_exercicio"] . '</h2>';
-                    echo '        <img class="imagem_card" src=' . $exercicio["imagem"] . ' alt="Imagem do exercício">';
+                    echo '        <img class="imagem_card" src=' . $exercicio["urlImagem"] . ' alt="Imagem do exercício">';
                     echo '        <p class="link_card">Mais informações +</p>';
                     echo '    </div>';
                     echo '</li>';
@@ -183,23 +183,36 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
 
         <div id="myModal" class="modal" onclick="fecharModal()">
             <div class="modal-content" onclick="event.stopPropagation();">
-                <span class="close" onclick="fecharModal()">&times;</span>
-                <h2 id="modalTitle"></h2>
-                <p id="modalDescricao"></p>
+                <span class="modal-content-titulo">
+                    <span class="close" onclick="fecharModal()">&times;</span>
+                    <h2 id="modalTitle"></h2>
+                </span>
+                <div id="modal-info">
+                    <img id="modalImagem" src="" alt="">
+                    <h3 class="modalSubtitulo">Músculos Envolvidos:</h3>
+                    <p id="modalMusculos"></p>
+                    <h3 class="modalSubtitulo">Instruções:</h3>
+                </div>
+                <iframe width="350" height="250" id="modalVideo" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
+
     </main>
 
     <script>
         // Funções do modal
-        function abrirModal(titulo, descricao) {
+        function abrirModal(titulo, musculos, urlImagem, urlVideo) {
             document.getElementById('modalTitle').innerText = titulo;
-            document.getElementById('modalDescricao').innerText = descricao;
+            document.getElementById('modalImagem').src = urlImagem;
+            document.getElementById('modalMusculos').innerHTML = musculos;
+            document.getElementById('modalVideo').src = urlVideo;
             document.getElementById('myModal').style.display = 'flex';
         }
 
         function fecharModal() {
             document.getElementById('myModal').style.display = 'none';
+            // Também é uma boa prática reiniciar o src do iframe ao fechar o modal para interromper a reprodução do vídeo
+            document.getElementById('modalVideo').src = '';
         }
     </script>
 </body>
